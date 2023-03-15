abeff4bcf5809a28fab3bea247e84fa7ed19852f
Daniela Bravo <dani@correo.com>
2023-03-15 16:37:43 -0400

    version basico de matriz char

---
+++ src/MatrizCaracter.java
@@ -0,0 +1,38 @@
+
+/**
+ *  Dada una matriz se desea obtener la matriz con la primera columa de ceros
+ * */
+
+public class MatrizCaracter {
+    static  char [][] matriz = {{'x','0','x','x'},
+                                {'x','0','x','x'},
+                                {'x','x','x','0'},
+                                {'x','x','x','0'}};
+
+    public static void main(String[] args) {
+        for (int fila = 0 ; fila < matriz.length ; fila++){
+            for (int col = 0 ; col < matriz[0].length ; col++){
+                if (matriz[fila][col] == '0'){
+                    intercambiarPrimeraColumna(fila,col);
+                }
+            }
+        }
+
+        imprimir();
+    }
+
+    private static void intercambiarPrimeraColumna(int fila,int col){
+        char aux = matriz[fila][0];
+        matriz[fila][0] = matriz[fila][col];
+        matriz[fila][col] = aux;
+    }
+
+    private static void imprimir(){
+        for (char [] array : matriz){
+            for (char elem : array){
+                System.out.print(elem+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
